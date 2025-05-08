f89bbfcfbb74a3c884f2b8c6070733a2ed14223f
shweta8814 <krishna1119850@gmail.com>
2025-05-08 15:06:42 +0000

    data type

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 1234
+let accountEmail = "Abc@gmail.com"
+var accountPassword = "0002"
+accountState = "Ambala"
+ 
+/*accountId = 0008
+1 after declaring const its value cannot be changed
+2 prefer not to use var because of issue in block-scope and functional-scope
+3 variables declare to types var,let
+*/
+console.table([accountId,accountId,accountPassword,accountState]); 
---
+++ 01_basics/02_data_type.js
@@ -0,0 +1,30 @@
+//"use strict"  treat all js code ass Newer version
+
+//alert( 3 + 3 )we are using nodejs not a browser//
+
+console.log(3+3)
+
+/* 
+1 code readablity should be high its proprly not a
+console.log(3
++3) is not be use this code writing
+console.log(3+3)this is used 
+
+2 ducmentry website tc.39.es/ecma this is original documentry website and 2 is mdn website
+*/
+let Name = "shwrta"
+let age = 23
+//string = textual data ko represent karta hai example: let name = "shweta";
+//number = numeric values ko represent krta hai example: let age = 23;
+//bigint = bahut bade number ko represent krta hai 
+//boolean = logical values ko represent karta hai example: true and false
+// null = ek intentional "empty" value ko represent karta hai example: let data = null;
+//undefined = jab variable deackare hote hai lekin value assign nhi ki jati example: let value; // undefined
+//symbol = unique aur immutable values ko represent karta hai jo primarily object properties ke liye use hota hai example let sym = symbol("unique")
+// object = key value pairs ko represent karta hai arrays, functions aur object iske example: let person ={name:"shweta" , age: 23};
+/*typeof opraters
+variable ko data type janne ke liye hota hai example: console.log(typeof name); //"string"
+*/
+
+
+console.log(typeof age)
